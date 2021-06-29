e156b2d8b5c8cb7bc81b6acb058e906120569656
subodh jaguri <subodhjaguri1@gmail.com>
2021-06-29 16:04:49 +0530

    product by id route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,4 +14,21 @@ router.get(
   })
 );
 
+// @desc  Fetch product by id
+// @route  Get  /api/products/:id
+// @access  Public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({
+        message: "Product not found",
+      });
+    }
+  })
+);
+
 export default router;
