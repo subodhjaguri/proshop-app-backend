9a52b7134197709558120e214ee419279f12e25a
subodh jaguri <subodhjaguri1@gmail.com>
2021-07-13 22:45:41 +0530

    register user conntroller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -25,12 +25,57 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// description register new user
+// route POST /api/users
+// access public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exixts");
+  }
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
+  if (user) {
+    res.status(201);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 // description Get User Profile
 // route       Get /api/users/profile
 // access      Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("success");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found ");
+  }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,29 +1,31 @@
 import jwt from "jsonwebtoken";
-import asyncHandler from "express";
+import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-  console.log("middleware");
   let token;
-  console.log(req.headers.authorization);
-  next();
-  //   if (
-  //     req.headers.authorization &&
-  //     req.headers.authorization.startsWith("Bearer")
-  //   ) {
-  // try {
-  //   token = req.headers.authorization.split(" ")[1];
-  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  //   console.log("decodedd", decoded);
-  //   next();
-  // } catch (error) {}
-  // }
-  // console.log(req.headers.authorization);
 
-  //   if (!token) {
-  //     res.status(401);
-  //     throw new Error("Not authorised, no token");
-  //   }
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorised, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorised, no token");
+  }
 });
 
 export { protect };
